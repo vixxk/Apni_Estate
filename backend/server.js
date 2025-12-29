2d4000ff7bd39abb28ff32da1875244043a4a1cd
Vivek-Anand727 <kamjorkismatwala@gmail.com>
2025-12-30 01:44:43 +0530

    redirect fix

--- backend/server.js
+++ backend/server.js
@@ -101,3 +101,9 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
